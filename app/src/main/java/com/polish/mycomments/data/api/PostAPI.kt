6a8be4cb4c767aae509d13ba392620e0d0e9b9f4
Osehi <osehiaseehilen@gmail.com>
2020-09-21 15:37:51 +0100

    homePageRepository created

--- app/src/main/java/com/polish/mycomments/data/api/PostAPI.kt
+++ app/src/main/java/com/polish/mycomments/data/api/PostAPI.kt
@@ -1,4 +1,37 @@
 package com.polish.mycomments.data.api
 
+import com.polish.mycomments.constants.URLConstants
+import com.polish.mycomments.constants.URLEndPoint
+import com.polish.mycomments.model.POSTItem
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
 interface PostAPI {
+
+    /*
+        this is the endpoint interface for the api
+     */
+    @GET(URLEndPoint.LIST_OF_POST)
+    suspend fun viewPosts():List<POSTItem>
+    /*
+        this function creates the web_service for the retrofit to make a network call
+     */
+    companion object {
+
+        operator fun invoke():PostAPI {
+
+            val client = OkHttpClient.Builder().build()
+            return Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(URLConstants.BASE_URL)
+                .client(client)
+                .build()
+                .create(PostAPI::class.java)
+
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/polish/mycomments/data/network/HomePageRepository.kt
@@ -0,0 +1,4 @@
+package com.polish.mycomments.data.network
+
+class HomePageRepository {
+}
